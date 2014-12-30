6959e543790ec0967cdab4164e043441c26c5433
Ind <mailtokasun@gmail.com>
2014-12-30 15:29:13 +0530

    minor changes of folder structure

---
+++ php-mysql-pdo-database-class/class.Database.inc.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * PHP/MySQL PDO Database Class
+ * @_thinkholic | 2014
+**/
+
+class Database 
+{
+    private static $dbName = '[DB NAME]' ; 
+    private static $dbHost = '[HOST NAME]' ;
+    private static $dbUsername = '[DB USER NAME]';
+    private static $dbUserPassword = '[DB PWD]';
+
+    private static $cont  = null;
+
+    public function __construct() 
+    {
+        die('Init function is not allowed');
+    }
+
+    public static function connect()
+    {
+       // One connection through whole application
+       if ( null == self::$cont )
+       {
+           try
+            {
+                self::$cont =  new PDO( "mysql:host=".self::$dbHost.";"."dbname=".self::$dbName, self::$dbUsername, self::$dbUserPassword);  
+            }
+            catch(PDOException $e) 
+            {
+                die($e->getMessage());  
+            }
+       } 
+
+       return self::$cont;
+    }
+
+    public static function disconnect()
+    {
+        self::$cont = null;
+    }
+}
+
+// EOF.
